66e1d1d7bea0fd46be6236330f7ce1d61132c1e6
Adam Berkovec <ajberk@gmail.com>
2015-02-03 15:43:49 -0800

    massive refactor, gemfile fixed

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if user
+      login(user)
+      redirect_to users_url(user)
+    else
+      flash.now[:errors] = ["Invalid username or password"]
+      render :new
+    end
+  end
+
+  def destroy
+    logout
+    render :new
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    render :show
+  end
+
+
+
+  private
+  def user_params
+    self.params.require(:user).permit(:username, :password)
+  end
+end
