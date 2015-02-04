7215b84000b3721a8de0e034961ead69ea2e4282
Adam Berkovec <ajberk@gmail.com>
2015-02-03 22:41:13 -0800

    had users_url(user) instead of user_url(user) in the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     )
     if user
       login(user)
-      redirect_to users_url(user)
+      redirect_to user_url(user)
     else
       flash.now[:errors] = ["Invalid username or password"]
       render :new
