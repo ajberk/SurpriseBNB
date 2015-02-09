e0e3d32eaa802ce18ab4de523d964dd35898fa9b
Adam Berkovec <ajberk@gmail.com>
2015-02-09 09:02:01 -0800

    trying to get things to work

---
+++ app/controllers/api/images_controller.rb
@@ -0,0 +1,21 @@
+class ImagesController < ApplicationController
+
+  def index
+    render json: Image.all
+  end
+
+  def create
+    # need to set the listing
+    listing = current_user.listings.find(params[:listing_id])
+    @image = listing.images.new(image_params)
+    if @image.save
+      render json: @image
+    else
+      render error: @image.errors.full_messages, status: :unprocessable_entity
+  end
+
+  private
+  def image_params
+    self.params.require(:image).permit(:image_url)
+  end
+end
