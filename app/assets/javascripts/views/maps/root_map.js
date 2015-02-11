f48e611bfe98a0b415a5f209c41537912931ff49
Adam Berkovec <ajberk@gmail.com>
2015-02-11 15:14:17 -0800

    working on getting pins on the listing locations on the map

--- app/assets/javascripts/views/maps/root_map.js
+++ app/assets/javascripts/views/maps/root_map.js
@@ -17,4 +17,63 @@ SurpriseBNBApp.Views.RootMap = Backbone.CompositeView.extend({
     this._map = new google.maps.Map(this.el, mapOptions);
     return this;
   },
+
+  // Event handlers
+  addMarker: function (listing) {
+    var data = listing.get("street_address") + " " + listing.get("city") + " " + listing.get("zipcode")
+    debugger
+
+    $.ajax({
+      url: 'https://maps.googleapis.com/maps/api/geocode/',
+      dataType: 'json',
+      data: data,
+      success: function(response) {
+        //response will have data corresponding to listing in the listings collection
+        debugger
+        response
+      }
+    })
+
+
+
+    if (this._markers[listing.id]) { return };
+    var view = this;
+
+    var latLng = new google.maps.LatLng(
+      listing.get('lat'),
+      listing.get('lng')
+    );
+
+    var marker = new google.maps.Marker({
+      position: latLng,
+      map: this._map,
+      title: listing.get('title')
+    });
+
+    google.maps.event.addListener(marker, 'click', function (event) {
+      view.showMarkerInfo(event, marker);
+    });
+
+    this._markers[listing.id] = marker;
+  },
+
+  removeMarker: function (listing) {
+    var marker = this._markers[listing.id];
+    marker.setMap(null);
+    delete this._markers[listing.id];
+  },
+
+  showMarkerInfo: function (event, marker) {
+    // This event will be triggered when a marker is clicked. Right now it
+    // simply opens an info window with the title of the marker. However, you
+    // could get fancier if you wanted (maybe use a template for the content of
+    // the window?)
+
+    var infoWindow = new google.maps.InfoWindow({
+      content: marker.title
+    });
+
+    infoWindow.open(this._map, marker);
+  }
+
 });
