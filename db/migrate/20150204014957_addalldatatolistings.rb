31a59331758b0dae2d50cbb2f764646cae643784
App Academy Student <student@appacademy.io>
2015-02-13 14:03:07 -0800

    Rewrite listings migration

--- db/migrate/20150204014957_addalldatatolistings.rb
+++ db/migrate/20150204014957_addalldatatolistings.rb
@@ -1,14 +1,19 @@
 class Addalldatatolistings < ActiveRecord::Migration
   def change
-    add_column :listings, :user_id,        :integer,  null:false
-    add_column :listings, :title,          :string,  null:false
-    add_column :listings, :start_date,     :date,  null:false
-    add_column :listings, :end_date,       :date,  null:false
-    add_column :listings, :country,        :string,  null:false
-    add_column :listings, :city,           :string,  null:false
-    add_column :listings, :street_address, :string,  null:false
-    add_column :listings, :zipcode,        :integer,  null:false
-    add_column :listings, :details,        :text,  null:false
+    create_table "listings" do |t|
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.integer  "user_id",        null: false
+      t.string   "title",          null: false
+      t.date     "start_date",     null: false
+      t.date     "end_date",       null: false
+      t.string   "country",        null: false
+      t.string   "city",           null: false
+      t.string   "street_address", null: false
+      t.integer  "zipcode",        null: false
+      t.text     "details",        null: false
+    end
+
     add_index(:listings, :country)
     add_index(:listings, :start_date)
     add_index(:listings, :end_date)
